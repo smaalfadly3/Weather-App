41b2bf643fdd7b281f074146c2007be7f41b3320
smaalfadly3 <smaalfadly@gmail.com>
2024-05-04 17:52:47 +0300

    init SiteNavigation component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    <RouterView></RouterView>
+  <div >
+    <SiteNavigation />
+    <RouterView />
 
   </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import SiteNavigation from './components/SiteNavigation.vue';
 </script>
 
 <style lang="scss" scoped>
