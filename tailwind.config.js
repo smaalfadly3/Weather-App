ca616be1de9d9b1e5b52da150a60d48cf086fa1a
smaalfadly3 <smaalfadly@gmail.com>
2024-05-04 17:11:26 +0300

    tailwindcss customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html ","./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "weather-primary": "#00668A",
+        "weather-secondary": "#004E71",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans -serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: "true",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
   },
   plugins: [],
 }
