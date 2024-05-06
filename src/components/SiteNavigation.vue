d7b294a08796f8df87ba7b07f838dbfbd14b1d90
smaalfadly3 <smaalfadly@gmail.com>
2024-05-06 12:46:12 +0300

    add transition styling to  BaseModal

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -34,7 +34,7 @@
                             This will save the city to view at a later time on the home page.
                         </li>
                     </ol>
-                    <h2>Removing a city</h2>
+                    <h2 class=" text-2xl">Removing a city:</h2>
                     <p>
                         If you no longer wish to track a city, simply select the city within the home page. At the bottom of the page, there will be an option to delete the city.
                     </p>
